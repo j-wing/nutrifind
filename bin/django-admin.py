1a7f3f194346e5ce54f1d7a2076a15ed9f46e2b7
Jordon Wing <jordonwii@gmail.com>
2014-12-22 18:20:42 -0800

    Removed virtualenv from the git repo.

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jordonwii/apps/nutrifind/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- nutrifind/main/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from main.models import Ingredient
-
-admin.site.register(Ingredient)
